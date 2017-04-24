a61d3c4d6028e51d6ad927cb3c6be8b8ae4f94d2
JeanMarcGoepfert <goepfertjeanmarc@gmail.com>
2017-04-24 13:09:39 +0000

    Initial tightly coupled version working

---
+++ README.md
@@ -0,0 +1,9 @@
+### Running
+
+`lein figwheel`
+
+`lein less auto`
+
+http://localhost:3449/
+
+Generated with: https://github.com/reagent-project/reagent-template
--- src/cljs/flash/omni-select.cljs
+++ src/cljs/flash/omni-select.cljs
@@ -1,24 +1,10 @@
-(ns flash.components
+(ns flash.omni-select
     (:require [reagent.core :as reagent :refer [atom]]
               [clojure.string :refer [starts-with? replace-first]]
               [reagent.session :as session]
               [flash.routes :refer [reference]]
-              [flash.omni-select :refer [omni-select]]
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
-
-(def verbs
-  {"comer" {"presento" {"yo" "como" "tu" "comes"}
-            "indefinido" {"yo" "comi" "tu" "comiste"}}
-   "habar" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "hablar" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "habars" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "hablarss" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "hablarsss" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "hablarssss" {"present" {"yo" "hablo" "tu" "hablas"}}
-   "hablarsssss" {"present" {"yo" "hablo" "tu" "hablas"}}
-   })
-
 
 (def db
   (reagent/atom {:verb-input ""
@@ -29,27 +15,10 @@
 (defn get-active-suggestion [db]
   (nth (db :suggestions) (db :option-tab-index) ""))
 
-(defn filter-verbs [input-value]
+(defn filter-verbs [verbs input-value]
   (if (empty? input-value)
     '()
     (filter #(starts-with? % input-value) (keys verbs))))
-
-(defn verb-list [verbs]
-  [:ul
-   (for [[subject verb] verbs]
-     [:li {:key subject}
-      [:span subject]
-      [:span verb]])])
-
-(defn home-page [])
-
-(defn reference-page []
-  (let [verb (get-in (session/get :active-route) [:params :verb])]
-    [:div [:h2 verb]
-     (for [[tense tenses] (verbs verb)]
-       [:div {:key (str verb "-" tense)}
-       [:h3 tense]
-       [verb-list tenses]])]))
 
 (defn submit-handler [e]
   (let [suggestion (get-active-suggestion @db)
@@ -89,10 +58,10 @@
                  (mod (inc-or-dec current-tab-index)
                       (count (@db :suggestions)))))))))
 
-(defn change-handler [value]
+(defn change-handler [value verbs]
   (do
     (swap! db assoc :verb-input value)
-    (swap! db assoc :suggestions (filter-verbs value))
+    (swap! db assoc :suggestions (filter-verbs verbs value))
     (swap! db assoc :option-tab-index 0)))
 
 (defn suggestion-box [input-value]
@@ -111,7 +80,7 @@
        ]
       )))
 
-(defn verb-search []
+(defn omni-select [verbs]
   (let [input-value (@db :verb-input)
         input-suggestion (get-active-suggestion @db)]
     [:form.verb-search {:on-submit #(submit-handler %)}
@@ -124,25 +93,9 @@
          }]]
       [:input.input.main-input
        {:on-key-down #(key-down-handler %)
-        :on-change #(change-handler (-> % .-target .-value))
+        :on-change #(change-handler (-> % .-target .-value) verbs)
         :value input-value
         :tab-index 0}]]
      [suggestion-box input-value]
      [:button {:type "submit"} "submit"]]))
 
-(defn current-page []
-  (let [{:keys [page params]} (session/get :active-route)
-        pages {:home-page home-page
-               :reference-page reference-page}
-        component (pages page)]
-    (if component
-      [:div [component params]]
-      [:div "sorry page not found"])))
-
-
-
-(defn app []
-  [:div
-   ;[omni-select verbs submit-hander]
-   [verb-search]
-   [current-page]])
